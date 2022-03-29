47bd82d7be50410d3cf6bfe7383ae09c845710a5
tianbiao <45967570+AmushereTB@users.noreply.github.com>
2022-03-29 16:10:44 +0200

    add routing for future purpose

--- LastProject.Client/src/components/App/index.tsx
+++ LastProject.Client/src/components/App/index.tsx
@@ -1,38 +1,16 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import './App.css';
-import RecipesBoard from "../RecipesBoard";
-import IRecipes from "../../Interfaces";
-import SearchForm from "../SearchForm";
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from "../Home";
 
-function App() {
-    const [recipeList, setRecipeList] = useState<IRecipes[]>([]);
-    const [searchTermOne, setSearchTermOne] = useState("");
-    const [searchTermTwo, setSearchTermTwo] = useState("");
-    const [searchTermThree, setSearchTermThree] = useState("");
-
-    const fetchRecipes = useCallback(async () => {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTermOne}`);
-        const data = await response.json();
-        const { meals } = data;
-        if(meals) 
-        {
-            setRecipeList(meals);
-        } else 
-        {
-            setRecipeList([]);
-        }
-    }, [searchTermOne])
-    
-    useEffect(() => {
-        fetchRecipes();
-    },[searchTermOne, fetchRecipes]);
-    
+const App = () => {
     return (
-    <main>
-      <SearchForm setSearchTermOne={setSearchTermOne} setSearchTermTwo={setSearchTermTwo} setSearchTermThree={setSearchTermThree} />
-      <RecipesBoard items={recipeList} />  
-    </main>
-  );
-}
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+            
+        </Router>
+    );
+};
 
 export default App;
