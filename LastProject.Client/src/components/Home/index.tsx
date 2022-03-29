ae634103379d607f282fd7b02bf32b2d592bbea5
tianbiao <45967570+AmushereTB@users.noreply.github.com>
2022-03-29 15:37:04 +0200

    searching works, add a little css

--- LastProject.Client/src/components/Home/index.tsx
+++ LastProject.Client/src/components/Home/index.tsx
@@ -1,10 +1,9 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import './App.css';
 import RecipesBoard from "../RecipesBoard";
 import IRecipes from "../../Interfaces";
 import SearchForm from "../SearchForm";
 
-function App() {
+function Home() {
     const [recipeList, setRecipeList] = useState<IRecipes[]>([]);
     const [searchTermOne, setSearchTermOne] = useState("");
     const [searchTermTwo, setSearchTermTwo] = useState("");
@@ -14,25 +13,25 @@ function App() {
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTermOne}`);
         const data = await response.json();
         const { meals } = data;
-        if(meals) 
+        if(meals)
         {
             setRecipeList(meals);
-        } else 
+        } else
         {
             setRecipeList([]);
         }
     }, [searchTermOne])
-    
+
     useEffect(() => {
         fetchRecipes();
     },[searchTermOne, fetchRecipes]);
-    
+
     return (
-    <main>
-      <SearchForm setSearchTermOne={setSearchTermOne} setSearchTermTwo={setSearchTermTwo} setSearchTermThree={setSearchTermThree} />
-      <RecipesBoard items={recipeList} />  
-    </main>
-  );
+        <main>
+            <SearchForm setSearchTermOne={setSearchTermOne} setSearchTermTwo={setSearchTermTwo} setSearchTermThree={setSearchTermThree} />
+            <RecipesBoard items={recipeList} />
+        </main>
+    );
 }
 
-export default App;
+export default Home;
