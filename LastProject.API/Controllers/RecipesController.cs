c618a132a2e5d768d5c9eff0fbd181d7a398a985
BeatriceBordignon <beatrice.bordignon@gmail.com>
2022-03-30 11:26:50 +0200

    Added DTO, fixed searchById

--- LastProject.API/Controllers/RecipesController.cs
+++ LastProject.API/Controllers/RecipesController.cs
@@ -60,62 +60,76 @@ namespace LastProject.API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetRecepeById (string id)
         {
-            var client = new RestClient("https://www.themealdb.com/api/json/v1/1/search.php?s=");
+            var client = new RestClient($"https://www.themealdb.com/api/json/v1/1/lookup.php?i={id}");
 
             var request = new RestRequest();
 
             var response = await client.ExecuteAsync(request);
 
-            var mealList = JsonSerializer.Deserialize<RecipesResponse>(response.Content);
+            var mealList = JsonSerializer.Deserialize<ListSearchIdDTO>(response.Content);
 
-            if(id == null)
+            if(string.IsNullOrEmpty(id))
                 {
-                    return Ok(mealList);
+                    return NotFound();
                 }
-
-            var result = mealList.ListOfRecipes;
             
-            var final = from r in result
-                    where r.idMeal == id
-                    select new SearchResultDTO{
-                        strMeal = r.strMeal,
-                        idMeal = r.idMeal,
-                        strMealThumb = r.strMealThumb
-                    };
-            return Ok(final);
+            return Ok(mealList);
+
+            // var result = mealList.ListOfRecipes;
+
+            // var final = (from r in result
+            //         where r.idMeal == id
+            //         select new SearchIdDTO{
+            //             strMeal = r.strMeal,
+            //             idMeal = r.idMeal,
+            //             strMealThumb = r.strMealThumb
+            //         }).ToList();
+            // return Ok(new ListSearchIdDTO {
+            //     ListOfRecipes = final});
         }
 
-
+        // , string searchByIngredient2, string searchByIngredient3
 
         [HttpGet("ingredients")]
-        public async Task<ActionResult> GetRecipeByIngredient (string searchByIngredient) //use param later
+        public async Task<ActionResult> GetRecipeByIngredient ( string searchByIngredient) //use param later
         {
             var client = new RestClient("https://www.themealdb.com/api/json/v1/1/search.php?s=");
 
             var request = new RestRequest();
 
             var response = await client.ExecuteAsync(request);
-
+            
             var mealList = JsonSerializer.Deserialize<RecipesResponse>(response.Content);
             
             // fix it with always full list in case invalid string 
-            if(searchByIngredient == null)
+            // if(string.IsNullOrEmpty(searchByIngredient) && string.IsNullOrEmpty(searchByIngredient2) && string.IsNullOrEmpty(searchByIngredient3) || string.IsNullOrEmpty(searchByIngredient))
+               
+               if(string.IsNullOrEmpty(searchByIngredient))
                 {
                     return Ok(mealList);
                 }
-
             
         var result = mealList.ListOfRecipes;
-            
-        var final = from r in result
+
+        // var searchStringLower = searchByIngredient.ToLower();
+        
+        var final = (from r in result
                     where r.Ingredients.Contains(searchByIngredient)
+                    // where r.Ingredients.Contains(searchByIngredient2)
+                    // where r.Ingredients.Contains(searchByIngredient3)
                     select new SearchResultDTO {
                         strMeal = r.strMeal,
                         idMeal = r.idMeal,
                         strMealThumb = r.strMealThumb
-                    };
+                    }).ToList();
 
-            return Ok(final);
+        
+
+            return Ok(new ListSearchDTO {
+                ListOfRecipes = final
+            });
+
+            //return Ok(final);
         }
 
 
