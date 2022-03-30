4f2d8e0834e523e03de2c4f6c68b2cea4ccb4a1f
BeatriceBordignon <beatrice.bordignon@gmail.com>
2022-03-30 17:25:00 +0200

    Added full 3 items search

--- LastProject.API/Controllers/RecipesController.cs
+++ LastProject.API/Controllers/RecipesController.cs
@@ -40,7 +40,7 @@ namespace LastProject.API.Controllers
         // , string searchByIngredient2, string searchByIngredient3
 
         [HttpGet("ingredients")]
-        public async Task<ActionResult> GetRecipeByIngredient ( string searchByIngredient) //use param later
+        public async Task<ActionResult> GetRecipeByIngredient (string searchByIngredient, string searchByIngredient2, string searchByIngredient3) //use param later
         {
             var client = new RestClient("https://www.themealdb.com/api/json/v1/1/search.php?s=");
 
@@ -53,29 +53,80 @@ namespace LastProject.API.Controllers
             // fix it with always full list in case invalid string 
             // if(string.IsNullOrEmpty(searchByIngredient) && string.IsNullOrEmpty(searchByIngredient2) && string.IsNullOrEmpty(searchByIngredient3) || string.IsNullOrEmpty(searchByIngredient))
                
-               if(string.IsNullOrEmpty(searchByIngredient))
-                {
-                    return Ok(mealList);
-                }
-            
-        var result = mealList.ListOfRecipes;
+            if(string.IsNullOrEmpty(searchByIngredient))
+            {
+                return Ok(mealList);
+            }
 
-        // var searchStringLower = searchByIngredient.ToLower();
-        
-        var final = (from r in result
-                    where r.Ingredients.Contains(searchByIngredient)
-                    // where r.Ingredients.Contains(searchByIngredient2)
-                    // where r.Ingredients.Contains(searchByIngredient3)
-                    select new SearchResultDTO {
-                        strMeal = r.strMeal,
-                        idMeal = r.idMeal,
-                        strMealThumb = r.strMealThumb
+            var result = mealList.ListOfRecipes;
+                
+            var finalDishIng1 = from r in result
+                        where r.Ingredients.Contains(searchByIngredient)
+                        select new SearchResultDTO {
+                            idMeal = r.idMeal,
+                            strMeal = r.strMeal,
+                            strMealThumb = r.strMealThumb
+                        };
+
+            var finalDishIng2 = from r in result
+                        where r.Ingredients.Contains(searchByIngredient2)
+                        select new SearchResultDTO {
+                            idMeal = r.idMeal,
+                            strMeal = r.strMeal,
+                            strMealThumb = r.strMealThumb
+                        };
+
+            var finalDishIng3 = (from r in result
+                        where r.Ingredients.Contains(searchByIngredient3)
+                        select new SearchResultDTO {
+                            idMeal = r.idMeal,
+                            strMeal = r.strMeal,
+                            strMealThumb = r.strMealThumb
+                        }).ToHashSet();
+
+            List<SearchResultDTO> Joining(IEnumerable<SearchResultDTO> param1, IEnumerable<SearchResultDTO> param2)
+            {
+                return (from f in param1
+                    join g in param2 on f.strMeal equals g.strMeal
+                    select new SearchResultDTO
+                    {
+                        idMeal = f.idMeal,
+                        strMeal = f.strMeal,
+                        strMealThumb = f.strMealThumb 
                     }).ToList();
+            }
 
-            return Ok(new ListSearchDTO {
-                ListOfRecipes = final
-            });
+            var twoIngredients = Joining(finalDishIng1, finalDishIng2);
+
+            var threeIngredients = Joining(twoIngredients, finalDishIng3);
+
+            if(threeIngredients.Count != 0)
+            {
+                return Ok(new ListSearchDTO {
+                    ListOfRecipes = threeIngredients
+                });
+            }
+            else if (twoIngredients.Count != 0)
+            {
+                return Ok(new ListSearchDTO {
+                    ListOfRecipes = twoIngredients
+                });
+            }
+            else
+            {
+                return Ok(new ListSearchDTO {
+                    ListOfRecipes = finalDishIng1.ToList()
+                });
+            }
         }
     }
 
 }
+            // var makeItWork = (from f in finalDishIng1
+            //                 join g in finalDishIng2 on f.strMeal equals g.strMeal
+            //                 select new SearchResultDTO
+            //                 {
+            //                     idMeal = f.idMeal,
+            //                     strMeal = f.strMeal,
+            //                     strMealThumb = f.strMealThumb 
+            //                 }).ToList();
